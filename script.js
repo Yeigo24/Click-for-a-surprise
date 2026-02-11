006973191f003d7ef82cebb066438b6155669f0a
Yeigo24 <diegolorenzo291@gmail.com>
2026-02-11 23:54:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+onload = () => {
+    document.body.classList.remove("container");
+  };
+
+  const wrapper = document.querySelector(".wrapper");
+  const openBtn = document.getElementById("openBtn");
+  const closeBtn = document.getElementById("closeBtn");
+  const song = document.getElementById("valentineSong");
+  
+  openBtn.addEventListener("click", () => {
+      wrapper.classList.add("open");
+      openBtn.style.display = "none";
+      closeBtn.style.display = "inline-block";
+     song.play();
+  });
+  
+  closeBtn.addEventListener("click", () => {
+      wrapper.classList.remove("open");
+      closeBtn.style.display = "none";
+      openBtn.style.display = "inline-block";
+        song.pause();
+        song.currentTime = 0;
+  });
+  
